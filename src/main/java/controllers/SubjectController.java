2f318f83f4c475bed3b10fb32d5c5b94fd6350cf
jose <msanchez.jose.94@gmail.com>
2020-04-07 00:07:33 -0300

    add basic service, repository and entity to persist

--- src/main/java/controllers/SubjectController.java
+++ src/main/java/controllers/SubjectController.java
@@ -1,13 +1,10 @@
-package com.example.restservice;
-
-import java.util.concurrent.atomic.AtomicLong;
+package controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GreetingController {
+public class SubjectController {
 
 	@GetMapping("/")
 	public String test() {
--- src/test/java/controllers/GreetingControllerTests.java
+++ src/test/java/controllers/GreetingControllerTests.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.example.restservice;
+package com.misaulasunq.controllers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -31,22 +31,22 @@ import org.springframework.test.web.servlet.MockMvc;
 @AutoConfigureMockMvc
 public class GreetingControllerTests {
 
-	@Autowired
-	private MockMvc mockMvc;
-
-	@Test
-	public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
-
-		this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
-				.andExpect(jsonPath("$.content").value("Hello, World!"));
-	}
-
-	@Test
-	public void paramGreetingShouldReturnTailoredMessage() throws Exception {
-
-		this.mockMvc.perform(get("/greeting").param("name", "Spring Community"))
-				.andDo(print()).andExpect(status().isOk())
-				.andExpect(jsonPath("$.content").value("Hello, Spring Community!"));
-	}
+//	@Autowired
+//	private MockMvc mockMvc;
+//
+//	@Test
+//	public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
+//
+//		this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
+//				.andExpect(jsonPath("$.content").value("Hello, World!"));
+//	}
+//
+//	@Test
+//	public void paramGreetingShouldReturnTailoredMessage() throws Exception {
+//
+//		this.mockMvc.perform(get("/greeting").param("name", "Spring Community"))
+//				.andDo(print()).andExpect(status().isOk())
+//				.andExpect(jsonPath("$.content").value("Hello, Spring Community!"));
+//	}
 
 }
