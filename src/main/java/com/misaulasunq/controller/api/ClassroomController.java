31acff92be082d26929746c57860834cc39f4521
Victor <flame.el22@gmail.com>
2020-05-20 23:21:51 -0300

    Agregado log4j2 y su configuracion
    Se agrego log4j2 par tracking y auditoria, se configuro parte de los patterns para los distintos niveles de logs.
    Falta configurar los patrones para trace, error y fatal.
    Falta agregar los logs a las clases que se crea necesarias.

--- src/main/java/com/misaulasunq/controller/api/ClassroomController.java
+++ src/main/java/com/misaulasunq/controller/api/ClassroomController.java
@@ -3,6 +3,9 @@ package com.misaulasunq.controller.api;
 import com.misaulasunq.service.ClassroomService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.apache.logging.log4j.Level;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,15 +29,26 @@ import java.util.List;
 @Api(tags = "Classroom Endpoint", value = "ClassroomEndpoint", description = "Controller para las aulas. Sugerencias, busquedas, etc.")
 public class ClassroomController {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     private ClassroomService classroomService;
+
+    public ClassroomController() {
+        LOGGER.info("Starting Classroom Controller");
+    }
 
     @GetMapping("/suggestions")
     @ApiOperation(value = "Devuelve una lista de sugerencia de aulas disponibles para buscar.")
     public ResponseEntity<List<String>> getSuggestions(){
-        return new ResponseEntity<List<String>>(
-            classroomService.retrieveClassroomSuggestions(),
-            HttpStatus.OK
+        LOGGER.info("Got a GET request to retrieve classroom suggestions");
+
+        ResponseEntity<List<String>> response = new ResponseEntity<>(
+                classroomService.retrieveClassroomSuggestions(),
+                HttpStatus.OK
         );
+
+        LOGGER.info("Responding the request with suggestions: {}", response);
+        return response;
     }
 }
