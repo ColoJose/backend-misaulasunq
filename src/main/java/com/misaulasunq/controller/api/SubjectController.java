94ded16917a8fab3286952033b94fc82fc033494
ColoJose <msanchez.jose.94@gmail.com>
2020-06-27 03:47:08 -0300

    Update SubjectController.java

--- src/main/java/com/misaulasunq/controller/api/SubjectController.java
+++ src/main/java/com/misaulasunq/controller/api/SubjectController.java
@@ -13,6 +13,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.support.PagedListHolder;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
